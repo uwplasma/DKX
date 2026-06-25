2ea4b5cae802b3c2a262811c47162a4d3ce7ec45
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2026-06-25 10:38:52 -0500

    Wire matrix-free derivatives into ambipolar Newton gates

--- tests/test_domain_package_import_contracts.py
+++ tests/test_domain_package_import_contracts.py
@@ -47,6 +47,7 @@ ACTIVE_PACKAGE_EXPORTS = {
         "implicit_linear_radial_current_derivative_from_builder",
         "implicit_matrix_free_radial_current_derivative",
         "implicit_matrix_free_radial_current_derivative_from_builder",
+        "matrix_free_radial_current_derivative_provider",
         "matrix_free_rhs1_vm_radial_current_linear_observable_system",
         "newton_ambipolar_root",
         "operator_tangent_from_centered_difference",
