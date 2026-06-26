86743f59a2f5fff7b2684c9a7226989ce1bc6836
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2026-06-26 09:30:38 -0500

    Flatten profile-response operator modules

--- tests/test_input_compat.py
+++ tests/test_input_compat.py
@@ -24,8 +24,8 @@ from sfincs_jax.input_compat import (
 from sfincs_jax.io import _resolve_equilibrium_file_from_namelist, localize_equilibrium_file_in_place, sfincs_jax_output_dict
 from sfincs_jax.namelist import read_sfincs_input
 from sfincs_jax.discretization.v3 import grids_from_namelist
-from sfincs_jax.operators.profile_response.fblock import _dphi_hat_dpsi_hat_from_er
-from sfincs_jax.operators.profile_response.system import full_system_operator_from_namelist
+from sfincs_jax.operators.profile_fblock import _dphi_hat_dpsi_hat_from_er
+from sfincs_jax.operators.profile_system import full_system_operator_from_namelist
 
 
 def test_shared_config_lookup_handles_namelists_and_nested_mappings() -> None:
