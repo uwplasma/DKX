9ad2aa01c15a976eaff2162c49bf26c816e5efaa
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2026-06-25 20:29:28 -0500

    Move PETSc binary reader into validation

--- examples/parity/collisionless_operator_matvec_parity.py
+++ examples/parity/collisionless_operator_matvec_parity.py
@@ -22,7 +22,7 @@ if str(_REPO_ROOT) not in sys.path:
 from sfincs_jax.collisionless import CollisionlessV3Operator, apply_collisionless_v3
 from sfincs_jax.indices import V3Indexing
 from sfincs_jax.namelist import read_sfincs_input
-from sfincs_jax.petsc_binary import read_petsc_mat_aij, read_petsc_vec
+from sfincs_jax.validation.petsc_binary import read_petsc_mat_aij, read_petsc_vec
 from sfincs_jax.v3 import geometry_from_namelist, grids_from_namelist
 
 
