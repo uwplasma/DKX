e655b6a601c58b17ce9586681a67a75a12ea5660
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2026-06-25 20:29:28 -0500

    Move PETSc binary reader into validation

--- examples/autodiff/autodiff_sensitivity_nu_n_scheme5.py
+++ examples/autodiff/autodiff_sensitivity_nu_n_scheme5.py
@@ -26,7 +26,7 @@ if str(_REPO_ROOT) not in sys.path:
     sys.path.insert(0, str(_REPO_ROOT))
 
 from sfincs_jax.namelist import read_sfincs_input
-from sfincs_jax.petsc_binary import read_petsc_vec
+from sfincs_jax.validation.petsc_binary import read_petsc_vec
 from sfincs_jax.v3_system import full_system_operator_from_namelist, residual_v3_full_system
 
 
